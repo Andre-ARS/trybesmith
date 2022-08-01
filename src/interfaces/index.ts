b2c8bb969acee494910c39d053ef7018df506d72
Andre <andre360.ars@gmail.com>
2022-07-31 22:17:15 -0300

    feat(interfaces): :sparkles: Creates the interface ILogin

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,9 +1,11 @@
 import IProduct from './IProduct';
 import IUser from './IUser';
 import IOrder from './IOrder';
+import ILogin from './ILogin';
 
 export {
   IProduct,
   IUser,
   IOrder,
+  ILogin,
 };
