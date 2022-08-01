6b89e87d5f1dfcefe50dfbb8e297c98322f134b7
Andre <andre360.ars@gmail.com>
2022-08-01 18:53:54 -0300

    feat(OrderService): :sparkles: Adds the method create ar the OrderService
    Adds a method, that creates a new order and updates the products
    
    Req08

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,18 +1,35 @@
 import { IOrder } from '../interfaces';
-import { connection, OrderModel } from '../models';
+import { connection, OrderModel, ProductModel } from '../models';
+import { ordersValidation } from '../validations';
 
 export default class OrderService {
-  public model: OrderModel;
+  public orderModel: OrderModel;
+
+  public productModel: ProductModel;
   
   constructor() {
-    this.model = new OrderModel(connection);
+    this.orderModel = new OrderModel(connection);
+    this.productModel = new ProductModel(connection);
   }
 
   public findAll = async (): Promise<IOrder[]> => {
-    const { model } = this;
+    const { orderModel } = this;
 
-    const orders = await model.findAll();
+    const orders = await orderModel.findAll();
 
     return orders;
+  };
+
+  public create = async (productsIds: number[], userId: number) => {
+    const { orderModel, productModel } = this;
+    ordersValidation({ productsIds });
+
+    const orderId = await orderModel.create(userId);
+
+    await productsIds.forEach(async (product: number) => {
+      await productModel.update(orderId, product);
+    });
+
+    return { userId, productsIds };
   };
 }
