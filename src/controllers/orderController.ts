39335d7284ca138981c511b946ba0dd37a98e0e3
Andre <andre360.ars@gmail.com>
2022-08-01 18:54:55 -0300

    feat(OrderController): :sparkles: Adds the controller create to create a new order

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -5,11 +5,20 @@ import { OrderService } from '../services';
 export default class OrderController {
   constructor(private orderService = new OrderService()) { }
 
-  public findAll = async (req: Request, res: Response) => {
+  public findAll = async (_req: Request, res: Response) => {
     const { orderService } = this;
         
     const orders = await orderService.findAll();
 
     res.status(StatusCodes.OK).json(orders);
+  };
+
+  public create = async (req: any, res: Response) => {
+    const { orderService } = this;
+    const { body: { productsIds }, user: { id } } = req;
+
+    const newOrder = await orderService.create(productsIds, id);
+
+    res.status(StatusCodes.CREATED).json(newOrder);
   };
 }
